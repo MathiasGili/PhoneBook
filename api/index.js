d77635b929a4da7c37cfe5e8322ee000e161d806
MGili <mathias.gilik@gmail.com>
2022-06-17 20:30:02 -0300

    Initial proyect

---
+++ api/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const cors = require("cors");
+const routes = require("./routes/routes")
+const express = require("express");
+const app = express();
+
+
+app.use(cors());
+app.use(express.static(__dirname + "/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(routes);
+app.listen(process.env.APP_PORT);
+
+console.log("Listening on " + process.env.APP_PORT)
